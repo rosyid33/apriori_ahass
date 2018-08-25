cd66d6a21c15d6e761b8d3b74bc2f628e4afdb52
Aunur Rasyid <rosyid33@gmail.com>
2018-08-25 10:53:18 +0700

    menu proses mining

--- layout.php
+++ layout.php
@@ -46,16 +46,19 @@ include_once 'fungsi.php';
         <!-- Google Fonts-->
         <link href="http://fonts.googleapis.com/css?family=Raleway:100,300,400" type="text/css" rel="stylesheet">
         <link href="http://fonts.googleapis.com/css?family=Roboto:400,300" type="text/css" rel="stylesheet">
+
+        <!-- daterange picker -->
+        <link rel="stylesheet" href="import/daterangepicker/daterangepicker-bs3.css">
         
         <style>
             #hornav ul > li > a:hover, #hornav ul > li > span:hover, #hornav ul > li > a.active, #hornav ul > li > span.active, #hornav ul > li > a.current, #hornav ul > li > span.current{
                 background: #f00;
             }
-            
+
             #hornav ul > li > a, #hornav ul > li > span{
                 background: #000;
             }
-            
+
             b, strong{
                 background: #000;
                 color: #fff;
@@ -63,7 +66,7 @@ include_once 'fungsi.php';
         </style>
     </head>
     <body>
-        
+
         <!-- Header -->
         <?php
         include "header.php";
@@ -71,54 +74,91 @@ include_once 'fungsi.php';
         <!-- End Header -->
         <!-- === END HEADER === -->
         <!-- === BEGIN CONTENT === -->
-        
-        <?php
-            $menu = ''; //variable untuk menampung menu
-            if (isset($_GET['menu'])) {
-                $menu = $_GET['menu'];
-            }
 
-            if ($menu != '') {
-                if (can_access_menu($menu)) {
-                    if (file_exists($menu . ".php")) {
-                        include $menu . '.php';
-                    } else {
-                        include "not_found.php";
-                    }
+        <?php
+        $menu = ''; //variable untuk menampung menu
+        if (isset($_GET['menu'])) {
+            $menu = $_GET['menu'];
+        }
+
+        if ($menu != '') {
+            if (can_access_menu($menu)) {
+                if (file_exists($menu . ".php")) {
+                    include $menu . '.php';
                 } else {
-                    include "forbidden.php";
+                    include "not_found.php";
                 }
-            } 
-            else {
-                include "home.php";
+            } else {
+                include "forbidden.php";
             }
+        } else {
+            include "home.php";
+        }
         ?>
         <!-- === END CONTENT === -->
         <!-- === BEGIN FOOTER === -->
         <!-- Footer -->
-        <?php 
+        <?php
         include "footer.php";
         ?>
         <!-- End Footer -->
-            <!-- JS -->
-            <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
-            <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-            <script type="text/javascript" src="assets/js/scripts.js"></script>
-            <!-- Isotope - Portfolio Sorting -->
-            <script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
-            <!-- Mobile Menu - Slicknav -->
-            <script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
-            <!-- Animate on Scroll-->
-            <script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
-            <!-- Stellar Parallax -->
-            <script type="text/javascript" src="assets/js/jquery.stellar.js" charset="utf-8"></script>
-            <!-- Sticky Div -->
-            <script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
-            <!-- Slimbox2-->
-            <script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
-            <!-- Modernizr -->
-            <script src="assets/js/modernizr.custom.js" type="text/javascript"></script>
-            <!-- End JS -->
+        <!-- JS -->
+        <!-- jQuery 2.1.4 -->
+        <script src="import/jQuery/jQuery-2.1.4.min.js"></script>
+        <!--<script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>-->
+        <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="assets/js/scripts.js"></script>
+        <!-- Isotope - Portfolio Sorting -->
+        <script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
+        <!-- Mobile Menu - Slicknav -->
+        <script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
+        <!-- Animate on Scroll-->
+        <script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
+        <!-- Stellar Parallax -->
+        <script type="text/javascript" src="assets/js/jquery.stellar.js" charset="utf-8"></script>
+        <!-- Sticky Div -->
+        <script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
+        <!-- Slimbox2-->
+        <script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
+        <!-- Modernizr -->
+        <script src="assets/js/modernizr.custom.js" type="text/javascript"></script>
+        <!-- End JS -->
+
+
+
+        <!-- jQuery 2.1.4 -->
+    <!-- <script src="import/jQuery/jQuery-2.1.4.min.js"></script> -->
+        <!-- date-range-picker -->
+        <script src="import/daterangepicker/moment-cloud.min.js"></script>
+        <script src="import/daterangepicker/daterangepicker.js"></script>
+
+        <!-- Page script -->
+        <script>
+            $(function () {
+                //Date range picker
+                $('#reservation').daterangepicker(
+                        {format: 'DD/MM/YYYY'}
+                );
+                $('#daterange-btn').daterangepicker(
+                        {
+                            ranges: {
+                                'Today': [moment(), moment()],
+                                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                            },
+                            startDate: moment().subtract(29, 'days'),
+                            endDate: moment()
+                        },
+                        function (start, end) {
+                            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                        }
+                );
+
+            });
+        </script>
     </body>
 </html>
 <!-- === END FOOTER === -->
